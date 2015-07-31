6e024b064513075cb20dd2c79205ecb14f7f72c3
cexoso <285516193@qq.com>
2015-07-31 18:19:23 +0800

    nd

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ var q = require('q');
 
 var app = express();
 app.use('/', express.static(__dirname + '/app'));
-console.log(__dirname)
 
+
+app.use(function (req, res, next) {
+  console.log('Time: %d', Date.now());
+  next();
+})
+
+app.get('/', function (req, res) {
+  res.send('Welcome');
+})
 console.log("服务器已启动,监听80")
 app.listen(80);
