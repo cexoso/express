503687d4ac802ff3a2712bfe736b7aff7d2f994d
cexoso <285516193@qq.com>
2015-08-10 18:20:43 +0800

    nd

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ var express = require('express');
 var q = require('q');
 
 var app = express();
-app.use('/', express.static(__dirname + '/app'));
+// app.use('/', express.static(__dirname + '/app'));
 
+app.set('views','views');
+app.set('view engine','jade');
 
 app.use(function (req, res, next) {
   console.log('Time: %d', Date.now());
   next();
 })
+
+app.get('/q', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
 
 app.get('/', function (req, res) {
   res.send('Welcome');
